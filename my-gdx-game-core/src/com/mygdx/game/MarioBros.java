22ca1b4ace304e3d19335d162f389b5e26e4a77e
dgerard <dgerard@L123OPE3.intra.jouve.com>
2016-04-28 19:33:05 +0200

    premier commit

---
+++ my-gdx-game-core/src/com/mygdx/game/MarioBros.java
@@ -0,0 +1,26 @@
+package com.mygdx.game;
+
+import screens.PlayScreen;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MarioBros extends Game {
+	
+	public static final int V_WIDTH = 400;
+	public static final int V_HEIGHT = 208;
+	public SpriteBatch batch;
+	Texture img;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		setScreen(new PlayScreen(this));
+	}
+
+	@Override
+	public void render () {
+		
+		super.render();		
+	}
+}
---
+++ my-gdx-game-core/src/screens/PlayScreen.java
@@ -0,0 +1,78 @@
+package screens;
+
+import scenes.Hud;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.mygdx.game.MarioBros;
+import com.sun.prism.image.ViewPort;
+
+public class PlayScreen implements Screen {
+	
+	private MarioBros game;
+	private OrthographicCamera gamecam;
+	private FitViewport gamePort;
+	private Hud hud;
+	
+	public PlayScreen(MarioBros game)
+	{
+		this.game = game;
+		gamecam = new OrthographicCamera();
+		gamePort = new FitViewport(MarioBros.V_WIDTH, MarioBros.V_HEIGHT, gamecam);
+		hud = new Hud(game.batch); 
+	}
+	
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(float delta) {
+
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
+		hud.stage.draw();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		gamePort.update(width, height);
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
